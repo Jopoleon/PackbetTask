afeb4bf3d76c807f5f87dbc1d473ea5298f9851d
Egor <egortictac@amil.ru>
2017-07-20 15:57:01 +0300

    second init

--- cards/main.go
+++ cards/main.go
@@ -9,127 +9,149 @@ import (
 )
 
 type Card struct {
-	Name  string
-	Value int
-	Suit  Suit
-}
-type Suit struct {
 	Name    string
+	Value   int
+	Suit    string
 	IsTrump bool
 }
 type PlayingCards struct {
 	Cards []Card
 }
 
-func main() {
-	var trump string
-	flag.StringVar(&trump, "trump", "Spades", "Specify what suit do you want to be the trump (Hearts, Diamonds, Clubs or Spades)")
-	flag.Parse()
-	if trump != "Hearts" || trump != "Diamonds" || trump != "Clubs" || trump != "Spades" {
-		log.Fatal("No such trump: ", trump, ". Use one of these (Hearts, Diamonds, Clubs or Spades)")
-	}
+type lessFunc func(p1, p2 *Card) bool
 
-	PC := GenerateCards(trump)
-	fmt.Println("Ampout of card:", len(PC.Cards))
-	for _, c := range PC.Cards {
-		fmt.Printf("before shuffle: %+v\n", c)
-	}
+// multiSorter implements the Sort interface, sorting the changes within.
+type multiSorter struct {
+	changes []Card
+	less    []lessFunc
+}
 
-	Shuffle(PC)
-	for _, c := range PC.Cards {
-		fmt.Printf("after shuffle: %+v\n", c)
-	}
-	sort.Sort(PC)
-	for i, c := range PC.Cards {
-		fmt.Printf("after SORT: %d %+v\n", i, c)
+// Sort sorts the argument slice according to the less functions passed to OrderedBy.
+func (ms *multiSorter) Sort(changes *[]Card) {
+	ms.changes = *changes
+	sort.Sort(ms)
+}
+
+// OrderedBy returns a Sorter that sorts using the less functions, in order.
+// Call its Sort method to sort the data.
+func OrderedBy(less ...lessFunc) *multiSorter {
+	return &multiSorter{
+		less: less,
 	}
 }
 
-//GenerateCards returns default deck of playing cards
+// Len is part of sort.Interface.
+func (ms *multiSorter) Len() int {
+	return len(ms.changes)
+}
+
+// Swap is part of sort.Interface.
+func (ms *multiSorter) Swap(i, j int) {
+	ms.changes[i], ms.changes[j] = ms.changes[j], ms.changes[i]
+}
+
+// Less is part of sort.Interface. It is implemented by looping along the
+// less functions until it finds a comparison that is either Less or
+// !Less. Note that it can call the less functions twice per call. We
+// could change the functions to return -1, 0, 1 and reduce the
+// number of calls for greater efficiency: an exercise for the reader.
+func (ms *multiSorter) Less(i, j int) bool {
+	p, q := &ms.changes[i], &ms.changes[j]
+	// Try all but the last comparison.
+	var k int
+	for k = 0; k < len(ms.less)-1; k++ {
+		less := ms.less[k]
+		switch {
+		case less(p, q):
+			// p < q, so we have a decision.
+			return true
+		case less(q, p):
+			// p > q, so we have a decision.
+			return false
+		}
+		// p == q; try the next comparison.
+	}
+	// All comparisons to here said "equal", so just return whatever
+	// the final comparison reports.
+	return ms.less[k](p, q)
+}
+
 func GenerateCards(trump string) PlayingCards {
 	var pc []Card
 	var CardNames = []string{"6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"}
-	var CardSuits = []*Suit{
-		{Name: "Hearts"},
-		{Name: "Diamonds"},
-		{Name: "Clubs"},
-		{Name: "Spades"},
-	}
+	var CardSuits = []string{"Hearts", "Diamonds", "Clubs", "Spades"}
 	//log.Printf("Suits: %+v", CardSuits)
-	if trump != "" {
-		CardSuits = withTrump(CardSuits, trump)
-	}
-	log.Printf("Suits: %+v", CardSuits)
+
+	//log.Printf("Suits: %+v", CardSuits)
 	for j := 0; j < len(CardSuits); j++ {
 		for i := 0; i < len(CardNames); i++ {
-			pc = append(pc, Card{Name: CardNames[i], Value: i + 1, Suit: *CardSuits[j]})
+			if CardSuits[j] == trump {
+				pc = append(pc, Card{Name: CardNames[i], Value: i + 1, Suit: CardSuits[j], IsTrump: true})
+			} else {
+				pc = append(pc, Card{Name: CardNames[i], Value: i + 1, Suit: CardSuits[j]})
+			}
+
 		}
 	}
-	pc = append(pc, Card{Name: "JokerRed", Value: 8, Suit: Suit{IsTrump: true}})
-	pc = append(pc, Card{Name: "JokerBlack", Value: 8, Suit: Suit{IsTrump: true}})
+	pc = append(pc, Card{Name: "JokerRed", Value: len(CardNames) + 1, IsTrump: true})
+	pc = append(pc, Card{Name: "JokerBlack", Value: len(CardNames) + 1, IsTrump: true})
 	return PlayingCards{Cards: pc}
 }
 
-// withTrump makes given suit trump
-func withTrump(CardSuits []*Suit, trump string) []*Suit {
-	for _, c := range CardSuits {
-		if c.Name == trump {
-			log.Println("True", c)
-			c.IsTrump = true
-		}
-	}
-	return CardSuits
-}
-
-//Shuffle shuffles cards in random order
-func Shuffle(data Interface) {
-	for i := data.Len() - 1; i > 0; i-- {
-		if j := rand.Intn(i + 1); i != j {
-			data.Swap(i, j)
+func Shuffle1(slc []Card) {
+	for i := 1; i < len(slc); i++ {
+		r := rand.Intn(i + 1)
+		if i != r {
+			slc[r], slc[i] = slc[i], slc[r]
 		}
 	}
 }
-func (c PlayingCards) Len() int {
-	return len(c.Cards)
-}
 
-func (c PlayingCards) Less(i, j int) bool {
+// ExampleMultiKeys demonstrates a technique for sorting a struct type using different
+// sets of multiple fields in the comparison. We chain together "Less" functions, each of
+// which compares a single field.
+func main() {
+	var trump string
+	flag.StringVar(&trump, "trump", "", "Specify what suit do you want to be the trump (Hearts, Diamonds, Clubs or Spades)")
+	flag.Parse()
+	if trump != "Hearts" && trump != "Diamonds" && trump != "Clubs" && trump != "Spades" && trump != "" {
+		log.Fatal("No such trump: ", trump, ". Use one of these (Hearts, Diamonds, Clubs or Spades)")
+	}
+	cards2 := GenerateCards(trump)
+	cards1 := cards2.Cards
+	Shuffle1(cards1)
+	// Closures that order the PlayingCards structure.
+	byName := func(c1, c2 *Card) bool {
+		return c1.Name < c2.Name
+	}
+	byValue := func(c1, c2 *Card) bool {
+		return c1.Value < c2.Value
+	}
+	byTrump := func(c1, c2 *Card) bool {
 
-	//check for Jokers
-	if c.Cards[i].Name == "JokerRed" || c.Cards[i].Name == "JokerBlack" {
+		if c1.IsTrump && !c2.IsTrump {
+			return false
+
+		}
+		if !c1.IsTrump && c2.IsTrump {
+			return true
+
+		}
 		return false
 	}
-	if c.Cards[j].Name == "JokerRed" || c.Cards[j].Name == "JokerBlack" {
-		return true
-	}
-	//if card is the trump
-	if c.Cards[i].Suit.IsTrump && c.Cards[j].Suit.IsTrump {
-		return c.Cards[i].Value < c.Cards[j].Value
 
-	}
-	if c.Cards[i].Suit.IsTrump && !c.Cards[j].Suit.IsTrump {
-		return false
+	OrderedBy(byName).Sort(&cards1)
+	fmt.Printf("By byName: %+v\n", cards1)
 
-	}
-	if !c.Cards[i].Suit.IsTrump && c.Cards[j].Suit.IsTrump {
-		return true
+	Shuffle1(cards1)
+	OrderedBy(byValue).Sort(&cards1)
+	fmt.Printf("By byValue: %+v\n", cards1)
 
-	}
-	//if card is not the trump
-	if !c.Cards[i].Suit.IsTrump && !c.Cards[j].Suit.IsTrump {
-		return c.Cards[i].Value < c.Cards[j].Value
+	Shuffle1(cards1)
+	OrderedBy(byTrump).Sort(&cards1)
+	fmt.Printf("By byTrump: %+v\n", cards1)
 
-	}
-	return c.Cards[i].Value < c.Cards[j].Value
-
-}
-
-func (c PlayingCards) Swap(i, j int) {
-	c.Cards[i], c.Cards[j] = c.Cards[j], c.Cards[i]
-}
-
-//interface fo shuffling
-type Interface interface {
-	Len() int
-	Swap(i, j int)
+	Shuffle1(cards1)
+	OrderedBy(byTrump, byValue).Sort(&cards1)
+	fmt.Printf("By byTrump>byValue: %+v\n", cards1)
 }
--- cards/main_test.go
+++ cards/main_test.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"log"
+	"sort"
 	"testing"
 )
 
-var AceOfSpades = Card{Name: "Ace", Value: 8, Suit: Suit{IsTrump: true}}
+var AceOfSpades = Card{Name: "Ace", Value: 8, Suit: "Spades", IsTrump: true}
 
 func TestGenerateCards(t *testing.T) {
 	trump := "Hearts"
@@ -15,7 +16,7 @@ func TestGenerateCards(t *testing.T) {
 		t.Fatalf("Bad number of cards: expected %s, got %s", expectedLength, len(PC.Cards))
 	}
 	for i, card := range PC.Cards {
-		if PC.Cards[i].Suit.Name == trump && !PC.Cards[i].Suit.IsTrump {
+		if PC.Cards[i].Suit == trump && !PC.Cards[i].IsTrump {
 			t.Fatalf("Some cards of trump suit are not trumps! : %+v", card)
 		}
 	}
@@ -28,18 +29,77 @@ func TestGenerateCards(t *testing.T) {
 		t.Fatalf("Bad number of cards: expected %s, got %s", expectedLength, len(PC.Cards))
 	}
 	for i, card := range PC.Cards {
-		if PC.Cards[i].Suit.Name == trump && !PC.Cards[i].Suit.IsTrump {
+		if PC.Cards[i].Name == trump && !PC.Cards[i].IsTrump {
 			t.Fatalf("Some cards of trump suit are not trumps! : %+v", card)
 		}
 	}
 	var expcetedAce = false
 	for i, card := range PC.Cards {
-		if PC.Cards[i].Name == "Ace" && PC.Cards[i].Value == 9 && PC.Cards[i].Suit.IsTrump {
+		if PC.Cards[i].Name == "Ace" && PC.Cards[i].Value == 9 && PC.Cards[i].IsTrump {
 			log.Println("Good, we got the AceOfSpades \\o/ , ", card)
 			expcetedAce = true
 		}
 	}
 	if !expcetedAce {
 		t.Fatalf("There is no AceOfSpade inside new deck!")
+	}
+}
+func TestShuffle1(t *testing.T) {
+	trump := "Spades"
+	PC := GenerateCards(trump)
+	cards := PC.Cards
+	Shuffle1(cards)
+
+	byValue := func(c1, c2 *Card) bool {
+		return c1.Value < c2.Value
+	}
+	//OrderedBy(byValue).Sort(&cards)
+	ms := multiSorter{changes: cards, less: OrderedBy(byValue).less}
+	if sort.IsSorted(&ms) {
+		t.Fatalf("cards are sorted but they do not must be s:\n %+v", cards)
+	}
+}
+func TestOrderedBy(t *testing.T) {
+	trump := "Spades"
+	PC := GenerateCards(trump)
+	cards := PC.Cards
+	Shuffle1(cards)
+	//byName := func(c1, c2 *Card) bool {
+	//	return c1.Name < c2.Name
+	//}
+	byValue := func(c1, c2 *Card) bool {
+		return c1.Value < c2.Value
+	}
+	byTrump := func(c1, c2 *Card) bool {
+		//if c1.IsTrump && c2.IsTrump {
+		//	return c1.Value < c2.Value
+		//}
+		if c1.IsTrump && !c2.IsTrump {
+			return false
+
+		}
+		if !c1.IsTrump && c2.IsTrump {
+			return true
+
+		}
+		return false
+	}
+	Shuffle1(cards)
+	OrderedBy(byValue).Sort(&cards)
+	ms := multiSorter{changes: cards, less: OrderedBy(byValue).less}
+	if !sort.IsSorted(&ms) {
+		t.Fatalf("OrderedBy(byValue) is not sorting cards:\n %+v", cards)
+	}
+	Shuffle1(cards)
+	OrderedBy(byTrump).Sort(&cards)
+	ms2 := multiSorter{changes: cards, less: OrderedBy(byTrump).less}
+	if !sort.IsSorted(&ms2) {
+		t.Fatalf("OrderedBy(byTrump) is not sorting cards:\n %+v", cards)
+	}
+	Shuffle1(cards)
+	OrderedBy(byTrump, byValue).Sort(&cards)
+	ms3 := multiSorter{changes: cards, less: OrderedBy(byTrump, byValue).less}
+	if !sort.IsSorted(&ms3) {
+		t.Fatalf("OrderedBy(byTrump,byValue) is not sorting cards: \n %+v", cards)
 	}
 }
